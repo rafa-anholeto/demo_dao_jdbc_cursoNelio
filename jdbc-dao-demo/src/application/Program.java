e240de5f0d91b5f5653b7e5a3837a952d1469b8b
rafa-anholeto <rafaanholeto3@gmail.com>
2021-10-08 15:46:32 -0300

    update implementation

--- jdbc-dao-demo/src/application/Program.java
+++ jdbc-dao-demo/src/application/Program.java
@@ -5,6 +5,7 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import javax.lang.model.util.SimpleElementVisitor6;
 import java.util.Date;
 import java.util.List;
 
@@ -35,5 +36,11 @@ public class Program {
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
         sellerDao.insert( newSeller);
         System.out.println("Inserted! New id = "+ newSeller.getId());
+
+        System.out.println("\n=== TEST 5: seller update ===");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
     }
 }
